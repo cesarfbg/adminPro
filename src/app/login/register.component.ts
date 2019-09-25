be66023339e43053500f830d69c8bf39c5f50ae0
César <cesarfbg10@gmail.com>
2019-09-25 17:31:35 -0500

    Primer commit

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class RegisterComponent {}
